76ff7d950344b0514f910bd094479111115f3329
Nicolas Frankel <nicolas@frankel.ch>
2021-04-03 12:13:54 +0200

    Refactor to a more functional style

--- src/main/kotlin/ch/frankel/blog/dataarrow/SpringDataArrowApplication.kt
+++ src/main/kotlin/ch/frankel/blog/dataarrow/SpringDataArrowApplication.kt
@@ -3,27 +3,40 @@ package ch.frankel.blog.dataarrow
 import java.time.LocalDate
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.CrudRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.function.*
 
 class Person(@Id val id: Long, var name: String, var birthdate: LocalDate?)
 
 interface PersonRepository : CrudRepository<Person, Long>
 
-@RestController
-class PersonController(private val repository: PersonRepository) {
+class PersonHandler(private val repository: PersonRepository) {
 
-    @GetMapping
-    fun getAll(): Iterable<Person> = repository.findAll()
+    fun getAll(req: ServerRequest) = ServerResponse.ok().body(repository.findAll())
+    fun getOne(req: ServerRequest): ServerResponse = repository
+        .findById(req.pathVariable("id").toLong())
+        .map { ServerResponse.ok().body(it) }
+        .orElse(ServerResponse.notFound().build())
+}
 
-    @GetMapping("/{id}")
-    fun getOne(@PathVariable id: Long) = repository.findById(id)
+fun beans() = beans {
+    bean<PersonHandler>()
+    bean {
+        val handler = ref<PersonHandler>()
+        router {
+            GET("/", handler::getAll)
+            GET("/{id}", handler::getOne)
+        }
+    }
 }
 
 @SpringBootApplication
 class SpringDataArrowApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringDataArrowApplication>(*args)
+    runApplication<SpringDataArrowApplication>(*args) {
+        addInitializers(beans())
+    }
 }
